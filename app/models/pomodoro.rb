2f023cbb18279fea64475b8829c85569260bba60
2dian718 <2dian718@gmail.com>
2018-10-29 23:20:18 +0800

    fix(timer): handle cancel timer logic

--- app/models/pomodoro.rb
+++ app/models/pomodoro.rb
@@ -30,7 +30,7 @@ class Pomodoro < ActiveRecord::Base
   end
 
   def start_pomodoro_notify
-    self.class.timer.after(POMODORO_INTERVAL_SECONDS) do
+    self.class.timer.after(self.id, POMODORO_INTERVAL_SECONDS) do
       text = "已经休息了 #{(POMODORO_INTERVAL_SECONDS/60).to_i} 分钟，请开始一个新的番茄吧！"
 
       pomodoro_app.create_message(vchannel_id, text)
@@ -38,9 +38,7 @@ class Pomodoro < ActiveRecord::Base
   end
 
   def finish_pomodoro_notify
-    self.class.timer.after(POMODORO_SECONDS) do
-      pomodoro_app.create_message(vchannel_id, "休息一下吧")
-
+    self.class.timer.after(self.id, POMODORO_SECONDS) do
       text = "已经工作了 #{(POMODORO_SECONDS/60).to_i} 分钟，如果番茄已完成，请发送 finish"
 
       pomodoro_app.create_message(vchannel_id, text)
@@ -48,22 +46,16 @@ class Pomodoro < ActiveRecord::Base
   end
 
   class Timer
-    def after(seconds, &block)
-      start
-
-      @timers.after(seconds, &block)
+    def initialize
+      @timers = {}
     end
 
-    def start
-      @timers ||= Timers::Group.new
+    def after(id, seconds, &blockn)
+      task = @timers[id]
 
-      Thread.new { run }
-    end
+      task.cancel if task
 
-    def run
-      loop do
-        @timers.wait
-      end
+      @timers[id] = Concurrent::ScheduledTask.execute(seconds, &blockn)
     end
   end
 end
